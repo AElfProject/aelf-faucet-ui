c273399b4a1534b946e1bf00d117e6130c2a6782
yongen.loong <yongen.loong@aelf.io>
2024-04-23 12:35:05 +0800

    fix: build error and remove console log

--- app/actions.ts
+++ app/actions.ts
@@ -3,19 +3,13 @@
 import { FormState } from "./form";
 
 export async function getToken(_prevState: FormState, formData: FormData) {
-  "use server";
-
   const address = formData.get("address");
-
-  console.log(address);
 
   const res = await fetch(
     `https://faucet.aelf.dev/api/claim?walletAddress=${address}`,
     { method: "POST" }
   );
-  console.log(res);
   const json = await res.json();
-  console.log(json);
 
   return json as {
     isSuccess: boolean;
