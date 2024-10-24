5f5353bf4100d9ae44f363047d508f78a3fd5b91
RutvikGhaskataEalf <rutikbhai.ghaskata@aelf.io>
2024-10-24 10:09:03 +0530

    feat: added google-recaptca verification on form

--- app/actions.ts
+++ app/actions.ts
@@ -15,11 +15,11 @@ export async function getToken(
 ): Promise<TMessageResult> {
   const address = formData.get("address");
   const choice = formData.get("choice");
+  const captchaToken = formData.get("captchaToken");
 
   try {
     const parsedAddress = addressSchema.parse(address);
     const parsedChoice: EChoices = choiceSchema.parse(choice);
-
     const res = await fetch(
       `https://faucet.aelf.dev/api/${
         {
@@ -27,11 +27,16 @@ export async function getToken(
           [EChoices.TOKEN]: "claim-seed",
           [EChoices.NFT]: "claim-nft-seed",
         }[parsedChoice]
-      }?walletAddress=${parsedAddress}`,
-      { method: "POST" }
+      }?walletAddress=${parsedAddress}&recaptchaToken=${captchaToken}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Platform: "FaucetUI", // Add the custom header here
+        },
+      }
     );
     const json = await res.json();
-
     const result = messageResultSchema.parse(json);
 
     return result;
