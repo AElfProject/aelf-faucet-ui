f25f922975e2a2c4b72f10da75adb2f74383a059
RutvikGhaskataEalf <rutikbhai.ghaskata@aelf.io>
2024-10-27 23:15:48 +0530

    fix: update api-url

--- app/actions.ts
+++ app/actions.ts
@@ -21,7 +21,7 @@ export async function getToken(
     const parsedAddress = addressSchema.parse(address);
     const parsedChoice: EChoices = choiceSchema.parse(choice);
     const res = await fetch(
-      `http://localhost:8801/api/${
+      `https://faucet.aelf.dev/api/${
         {
           [EChoices.ELF]: "claim",
           [EChoices.TOKEN]: "claim-seed",
